adc6a68d45f031e87e4365f4c6790616a35f7df2
Jon Ubuntu <jon_portugalete@hotmail.com>
2022-10-30 12:48:32 +0100

    Forked again in main function.

--- Source_files/fork.c
+++ Source_files/fork.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main()
+/*
+@brief Simple function that prints a different output depending on the input value.
+@param retfork Integer value returned by fork function.
+@return Void.
+*/
+void greet(int retFork)
 {
-	int retFork = fork();
-
-	printf("Hello World! Process ID: %d\r\n", getpid());
-
 	if (retFork == 0)
 	{
 		printf("I'm the child process!\r\n");
@@ -15,6 +16,23 @@ int main()
 	{
 		printf("I'm the parent process!\r\n");
 	}
+}
+
+int main()
+{
+	// Fork once.
+	int retFork = fork();
+
+	printf("Hello World! Process ID: %d\r\n", getpid());
+
+	greet(retFork);
+
+	// Fork again.
+	retFork = fork();
+
+	printf("Hi again! Process ID: %d\r\n", getpid());
+
+	greet(retFork);
 
 	return 0;
 }
