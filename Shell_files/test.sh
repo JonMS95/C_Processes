8636ba0a1c5b7aaf239ac8654abf570c12b57165
Jon Ubuntu <jon_portugalete@hotmail.com>
2022-11-04 18:13:46 +0100

    Created test.sh script. Moved .sh scripts to "Shell_scripts" directory.

---
+++ Shell_files/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo
+echo "*******************************"
+echo "Testing 'fork' executable file."
+echo "*******************************"
+./Executable_files/fork
+
+sleep 1
+
+echo
+echo "*******************************"
+echo "Testing 'exec' executable file."
+echo "*******************************"
+./Executable_files/exec
+sleep 1
+echo "*******************************"
